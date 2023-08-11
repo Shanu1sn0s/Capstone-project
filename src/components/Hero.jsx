3f7037c53ca50985ad70e7a604aa994e4fe9fe0b
Olayinka Shanu <ademayowa4@gmail.com>
2023-08-11 23:50:36 +0800

    updated

---
+++ src/components/Hero.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import Line from "../assets/bottom stripe.png";
+import Back from "../assets/Bg back.png";
+import Base from "../assets/Bg bottom.png";
+import Arrow from "../assets/Line 7.png";
+import LinkImage from "../assets/link-2.png";
+import CircleBG from "../assets/circle bg.png";
+
+const Hero = () => {
+  return (
+    <div className="w-full h-screen relative top-32 text-center ">
+      {/* optimize */}
+      <div className="mt-5 max-w-4xl  flex flex-cols-2 font-[600] items-center mx-auto">
+        <h1 className="text-4xl font-700 leading-relaxed">
+          Optimize Your Online Experience with Our Advanced
+          <span className="text-blue-600 text-center w-2 ml-2">
+            URL Shortening{" "}
+            <img
+              className="w-[120px] ml-[450px] mt-[-10px] "
+              src={Line}
+              alt=""
+            />
+          </span>
+          Solution
+        </h1>
+      </div>
+      {/* personalize paragraph  with button div inside*/}
+      <div className="mt-5 max-w-xl  flex flex-col gap-3 items-center mx-auto">
+        <p className="text-xl w-[670px]">
+          Personalize your shortened URLs to align with your brand identity.
+          Utilize custom slugs, branded links, and domain customization options
+          to reinforce your brand presence and enhance user engagement.
+        </p>
+        {/* button div */}
+        <div className="flex gap-7">
+          <button className="px-7">Sign Up</button>
+          <button className="bg-transparent text-blue-600 border-none">
+            Learn more
+          </button>
+        </div>
+      </div>
+      {/* background image design div */}
+      <div className="w-full h-screen grid justify-center text-center">
+        {/* card and bg image */}
+        <div className="flex justify-center text-center m-auto mt-40">
+          {/* card */}
+          <div className="border-blue-600 border w-[580px] h-[300px] ml-[390px] bg-zinc-50 grid justify-center m-auto text-center py-20 px-10 rounded-[20px]">
+            {/* illustrations */}
+            <div className="flex justify-center mt-[-20px] gap-5 ">
+              <img src={LinkImage} alt="" />
+              <img src={LinkImage} alt="" />
+              <img src={LinkImage} alt="" />
+              <img className="h-5 mt-7" src={Arrow} alt="" />
+              <img src={LinkImage} alt="" />
+            </div>
+            {/* illustrations ends */}
+
+            {/* Card Paragraph */}
+            <div className="mt-5">
+              <p className="text-xl">
+                Seamlessly transform your long URLs into 
+                <span className="font-[700] ml-2">concise</span> and  
+                <span className="font-[700] ml-2">shareable links</span> with just a
+                few clicks.
+              </p>
+            </div>
+          </div>
+          {/* bg image */}
+          <div className="">
+            <img
+              className="z-[-1] relative left-[-750px] top-[-70px]"
+              src={Back}
+              alt=""
+            />
+          </div>
+        </div>
+        {/* card and bg image end */}
+        {/* buttom image */}
+        <div className="w-[900px] flex justify-center text-center m-auto">
+          <img src={Base} alt="" />
+        </div>
+        {/* buttom image end */}
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React,{useState} from "react";
 import Logo from "../assets/Logo.png";
 import Line from "../assets/Logo line.png";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
+
 const Navbar = () => {
+const[nav,setNav]=useState(false);
+const handleClick =() => setNav(!nav)
+
+
   return (
-    <div className="w-screen h-[80px] z-10 fixed bg-white drop-shadow-md rounded-[50px] my-[10px]">
+    <div className=" w-screen h-[80px] z-10 fixed mt-[-20px] bg-white drop-shadow-md rounded-[50px] my-[10px] ">
       <div className="px-2  py-4 flex justify-between items-center w-full h-full">
         <div className="flex items-center">
           <img className="px-2  py-4 w-9" src={Logo} alt="" />
@@ -27,7 +32,29 @@ const Navbar = () => {
           </button>
           <button>Try for Free</button>
         </div>
+      <div className="md:hidden md:mr-[500px] duration-300" onClick={handleClick}>
+      {!nav ?<MenuIcon className=" mr-5 h-8 w-8 text-blue-600"  />
+      : <XIcon className="md:hidden mr-5 h-8 w-8 text-blue-600"  />}
+         
+      </div>     
       </div>
+       {/* Mobile menu */}
+<ul className={!nav ? "hidden": "md:hidden absolute text-white text-center bg-blue-600 w-full px-8 py-4 duration-300"}>
+  <li className="border-white w-full hover:bg-gray-300 hover:text-blue-600 rounded-[40px] ease-in-out duration-300">My Url</li>
+  <li className="border-white w-full hover:bg-gray-300 hover:text-blue-600 rounded-[40px] ease-in-out duration-300">Features</li>
+  <li className="border-white w-full hover:bg-gray-300 hover:text-blue-600 rounded-[40px] ease-in-out duration-300">Pricing</li>
+  <li className="border-white w-full hover:bg-gray-300 hover:text-blue-600 rounded-[40px] ease-in-out duration-300">FAQs</li>
+<div className="flex flex-col my-4">
+<button className="bg-white hover:text-white text-blue-600 px-8 py-3 mb-4">
+Log in
+</button>
+<button className="px-8 py-3 border-white hover:text-white">
+Try for Free
+</button>
+</div>
+  
+</ul>
+
     </div>
   );
 };
