bcc570109b91d7e3099395fc486fe2cc7bbeb38d
Olayinka Shanu <ademayowa4@gmail.com>
2023-10-12 18:46:31 +0800

    New

--- src/Pages/Shortner.jsx
+++ src/Pages/Shortner.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 import Footer from "../components/Footer";
+import Result from "../components/Result";
 
 function Shortner() {
   const { user, logOut } = UserAuth();
@@ -18,15 +19,23 @@ function Shortner() {
   return (
     <div>
       <div className="grid my-10 justify-center gap-10">
-        <h1 className="font-bold text-3xl">Shortener</h1>
+        <h1 className="font-bold text-3xl text-blue-700">Scissor</h1>
         <p className="font-bold text-1xl">Welcome, {user?.displayName}</p>
 
+      
+      </div>
+      <div className="flex justify-center my-auto mx-auto">
+        <input className="p-2 border text-blue-500" type="text" placeholder="Your url goes here" />
+        <button className="rounded-none">Shorten</button>
+      </div>
+        <Result/>
+        <div className="mx-3">
         {user?.displayName ? (
           <button onClick={handleSignOut} className="boreder rounded-none">Logout</button>
         ) : (
           navigate("/login")
         )}
-      </div>
+        </div>
       <Footer />
     </div>
   );
